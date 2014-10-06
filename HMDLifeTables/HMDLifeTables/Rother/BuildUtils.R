b33d4f3d32b729c16bb745d8f4230a9c0bb72309
triffe <triffe@demog.berkeley.edu>
2014-10-06 14:33:17 -0700

    added man files for m0 -> a0 method. Coefs will probably still need to be updated

--- HMDLifeTables/HMDLifeTables/Rother/BuildUtils.R
+++ HMDLifeTables/HMDLifeTables/Rother/BuildUtils.R
@@ -3,7 +3,9 @@
 ###############################################################################
 
 # this builds the package, incrementing the version automatically
-BuildRLifeTablePackage <- function(MPVERSION = 6, MPVERSION.origin = "2013-01-01", commit = TRUE){
+BuildRLifeTablePackage <- function(MPVERSION = 6, MPVERSION.origin = "2013-01-01", 
+  
+  commit = TRUE){
   
   # get time difference from origin
   increment <-  as.numeric(
@@ -16,18 +18,18 @@ BuildRLifeTablePackage <- function(MPVERSION = 6, MPVERSION.origin = "2013-01-01
   pkg.vs    <- paste0("Version: ",MPVERSION, ".", as.character(round(increment / 365.25, digits = 4)))
   
   # update version in DESCRIPTION file:
-  DESC      <- readLines("/data/commons/triffe/git/HMD_Rlifetables_git/RLifeTables/DESCRIPTION")
+  DESC      <- readLines("/data/commons/triffe/HMDLifeTables/HMDLifeTables/HMDLifeTables/DESCRIPTION")
   Version.i <- grep(DESC, pattern = "Version:")
   DESC[Version.i] <- pkg.vs
-  writeLines(DESC, "/data/commons/triffe/git/HMD_Rlifetables_git/RLifeTables/DESCRIPTION")
+  writeLines(DESC, "/data/commons/triffe/git/HMDLifeTables/HMDLifeTables/HMDLifeTables/DESCRIPTION")
   
   if (commit){
     # commit package changes automatically:
-    system(paste("cd /data/commons/triffe/COMMONS/git/HMD_Rlifetables_git/RLifeTables \n git commit -m ","'Package rebuild ",pkg.vs, "'"))
+    system(paste("cd /data/commons/triffe/COMMONS/git/HMDLifeTables/HMDLifeTables/HMDLifeTables \n git commit -m ","'Package rebuild ",pkg.vs, "'"))
   }
   # build
-  devtools::build(pkg = "/data/commons/triffe/git/HMD_Rlifetables_git/RLifeTables",
-    path = "/data/commons/triffe/git/HMD_Rlifetables_git/RLifeTablesBuilds")
+  devtools::build(pkg = "/data/commons/triffe/git/HMDLifeTables/HMDLifeTables/HMDLifeTables",
+    path = "/data/commons/triffe/git/HMDLifeTables/HMDLifeTables/HMDLifeTables")
 }
 
 # what's the newest build available?
