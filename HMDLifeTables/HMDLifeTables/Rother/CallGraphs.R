a7e19e1adc86cb658f16208b43f9d1e740586d76
Tim Riffe <tim.riffe@gmail.com>
2016-05-24 09:54:39 +0200

    .

--- HMDLifeTables/HMDLifeTables/Rother/CallGraphs.R
+++ HMDLifeTables/HMDLifeTables/Rother/CallGraphs.R
@@ -1,16 +1,22 @@
 
 # Author: triffe
 ###############################################################################
-#install.packages("mvbutils")
+#?install.packages
+#install.packages("/home/tim/git/HMDLifeTables/HMDLifeTables/HMDLifeTables_6.2.4752.tar.gz",
+#type="source",repos=NULL)
 CallGraph <- function(LTfunction = "ltper_AxN"){
-  mvbutils::foodweb(where = "package:RLifeTable", prune = LTfunction, generics = c("ExtractXXXfromWORKING"),
+  mvbutils::foodweb(where = "package:HMDLifeTables", prune = LTfunction, generics = c("ExtractXXXfromWORKING"),
     boxcolor = gray(.95))
   title(paste0(LTfunction, "() call tree"))
 }
-
-# these may need resizing to be legible
+rownames(installed.packages())
+#devtools::install_github("mvbutils")
+# these may need resizing to be legible\library(HMDLifeTables)
 CallGraph("ltcoh_AxN")
 CallGraph("ltcohBoth_AxN")
 CallGraph("ltper_AxN")
 CallGraph("ltperBoth_AxN")
 CallGraph("RunHMDCountry")
+CallGraph("perTadj")
+
+
